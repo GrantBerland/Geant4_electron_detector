8d13770e47158c810c380f668e23b95740ce3134
grant <grant.berland.gb@gmail.com>
2018-08-29 14:27:17 -0600

    everything working at the moment

--- analysis/fit_gauss_main.py
+++ analysis/fit_gauss_main.py
@@ -12,16 +12,29 @@ detector2_hits = pd.read_csv("./data/hits_det2.csv",
                              names=["x", "y", "z", "mom_x", "mom_y", "mom_z"],
                              dtype=np.float64)
 
-Xin = detector1_hits["x"]
-Yin = detector1_hits["z"]
+X1 = detector1_hits["x"]
+Y1 = detector1_hits["z"]
 
-
-counts,xbins,ybins,image = plt.hist2d(Xin,Yin,bins=200)
+plt.figure()
+counts,xbins,ybins,image = plt.hist2d(X1,Y1,bins=300)
 plt.colorbar()
-plt.contour(counts.transpose())#,extent=[xbins.min(),xbins.max(),ybins.min(),ybins.max()],linewidths=1)
+plt.contour(counts.transpose(),extent=[xbins.min(),xbins.max(),ybins.min(),ybins.max()],linewidths=1)
 plt.xlim([-5,5]); plt.xlabel("x [cm]")
 plt.ylim([-5,5]); plt.ylabel("z [cm]")
+plt.title("Detector 1")
 
+# Hit data from detector 2
+X2 = detector2_hits["x"]
+Y2 = detector2_hits["z"]
+
+
+plt.figure()    # New figure
+counts,xbins,ybins,image = plt.hist2d(X2,Y2,bins=300)   # Creates histogram from hit data
+plt.colorbar()
+plt.contour(counts.transpose(),extent=[xbins.min(),xbins.max(),ybins.min(),ybins.max()],linewidths=1)
+plt.xlim([-5,5]); plt.xlabel("x [cm]")
+plt.ylim([-5,5]); plt.ylabel("z [cm]")
+plt.title("Detector 2")
 
 '''
 data = fit.gaussian(3, 100, 100, 20, 40)(xbins, ybins)
