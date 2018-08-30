23c3bf6f150d86151b7f40da1619253f2b75deff
grant <grant.berland.gb@gmail.com>
2018-08-30 16:45:30 -0600

    fitting procedure working well, next step to automate analysis

--- analysis/fit2Dgaussian.py
+++ analysis/fit2Dgaussian.py
@@ -22,25 +22,72 @@ Y1 = detector1_hits["z"]
 X2 = detector2_hits["x"]
 Y2 = detector2_hits["z"]
 
-(mu_nr, sigma1_nr,
-     sigma2_nr, alpha_nr) = fit_bivariate_normal(X1, Y1, robust=True)
+###########################################
+### astroML's robust fit for detector 1 ###
+###########################################
+
+(mu_nr1, sigma1_nr1,
+     sigma2_nr1, alpha_nr1) = fit_bivariate_normal(X1, Y1, robust=True)
 
 fig = plt.figure()
 ax = fig.add_subplot(1,2,1)
 
 # Scatter plot of hit data
-ax.scatter(X1, Y1, s=2, lw=0, c='k', alpha=0.5)
-sigma_conf = 3
+ax.scatter(X1, Y1, s=2, lw=0, c='k', alpha=0.25)
 
-E = Ellipse(mu_nr, sigma1_nr * sigma_conf, sigma2_nr * sigma_conf,
-            (alpha_nr * 180. / np.pi), ec='k', fc='none', linestyle='dotted')
+E_2sigma = Ellipse(mu_nr1, sigma1_nr1 * 2, sigma2_nr1 * 2,
+            (alpha_nr1 * 180. / np.pi), ec='b', fc='none', linestyle='dotted')
 
-ax.add_patch(E)
+ax.add_patch(E_2sigma)
+
+E_3sigma = Ellipse(mu_nr1, sigma1_nr1 * 3, sigma2_nr1 * 3,
+            (alpha_nr1 * 180. / np.pi), ec='r', fc='none', linestyle='dotted')
+
+ax.add_patch(E_3sigma)
+
 ax.set_xlim(-5, 5)
 ax.set_ylim(-5, 5)
 ax.set_xlabel("x [cm]")
 ax.set_ylabel("z [cm]")
 ax.text(0.04, 0.96, "Detector 1",
             ha='left', va='top', transform=ax.transAxes)
+
+ax.plot([0], [0], '-b', label='2 sigma')
+ax.plot([0], [0], '-r', label='3 sigma')
+ax.legend(loc='lower right')
+
+
+###########################################
+### astroML's robust fit for detector 2 ###
+###########################################
+
+(mu_nr2, sigma1_nr2,
+     sigma2_nr2, alpha_nr2) = fit_bivariate_normal(X2, Y2, robust=True)
+
+ax = fig.add_subplot(1,2,2)
+
+# Scatter plot of hit data
+ax.scatter(X1, Y1, s=2, lw=0, c='k', alpha=0.25)
+
+E_2sigma = Ellipse(mu_nr2, sigma1_nr2 * 2, sigma2_nr2 * 2,
+            (alpha_nr2 * 180. / np.pi), ec='b', fc='none', linestyle='dotted')
+
+ax.add_patch(E_2sigma)
+
+E_3sigma = Ellipse(mu_nr2, sigma1_nr2 * 3, sigma2_nr2 * 3,
+            (alpha_nr2 * 180. / np.pi), ec='r', fc='none', linestyle='dotted')
+
+ax.add_patch(E_3sigma)
+
+ax.set_xlim(-5, 5)
+ax.set_ylim(-5, 5)
+ax.set_xlabel("x [cm]")
+ax.set_ylabel("z [cm]")
+ax.text(0.04, 0.96, "Detector 2",
+            ha='left', va='top', transform=ax.transAxes)
+
+ax.plot([0], [0], '-b', label='2 sigma')
+ax.plot([0], [0], '-r', label='3 sigma')
+ax.legend(loc='lower right')
 
 plt.show()
